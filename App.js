ef6f43987b69c1cea057f5ba90c2d63ca2c89086
Thiago <thiagognovaes@gmail.com>
2020-07-09 13:38:29 -0300

    Lembrete app feito em  aula

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+
+import LembreteItem from './src/components/LembreteItem';
+import LembreteInput from './src/components/LembreteInput';
+
 
 export default function App() {
+
+  //const [lembrete, setLembrete] = useState('');
+  const [lembretes, setLembretes] = useState([]);
+
+  const [contadorLembretes, setContadorLembretes] = useState(0);
+
+  //const capturarLembrete = (digitado) => {
+  //   setLembrete(digitado);
+  // }
+
+  const adicionarLembrete = (lembrete) => {
+    setLembretes(lembretes => {
+      setContadorLembretes(contadorLembretes + 1);
+      return [{ key: contadorLembretes.toString(), value: lembrete }, ...lembretes];
+    })
+    console.log(lembrete);
+  }
+
+  const apagarLembretes = () => {
+    setLembretes([]);
+  }
+
+  const removerLembrete = (keyASerRemovida) => {
+    setLembretes(lembretes => {
+      return lembretes.filter((lembrete) => {
+        return lembrete.key !== keyASerRemovida
+      })
+    })
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.telaPrincipalView}>
+
+      <LembreteInput onAdicionarLembrete={adicionarLembrete} onApagarTudo={apagarLembretes} />
+
+      <View>
+        <FlatList
+          data={lembretes}
+          renderItem={
+            lembrete => (
+              <LembreteItem
+                chave={lembrete.item.key}
+                lembrete={lembrete.item.value}
+                onDelete={removerLembrete} />
+            )
+          }
+        />
+        {/*aqui será exibida a lista de lembretes
+          <ScrollView>
+            {
+              lembretes.map(lembrete =>
+                <View
+                  key={lembrete}
+                  style={styles.itemNaLista}>
+                  <Text style={{ fontSize: 16, textAlign: 'center' }}>{lembrete}</Text>
+                </View>
+              )
+            }
+          </ScrollView>
+          */
+        }
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  itemNaLista: {
+    padding: 16,
+    backgroundColor: '#EEE',
+    borderColor: '#000',
+    borderWidth: 1,
+    marginBottom: 8,
+    borderRadius: 12,
+    fontSize: 16,
+    width: '80%',
+    alignSelf: 'center'
   },
+  telaPrincipalView: {
+    padding: 50
+  },
+  lembreteView: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12
+  },
+  lembreteTextInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 2
+  }
 });
